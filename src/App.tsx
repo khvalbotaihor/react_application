743c9c414dc667b8a6612a44217fd98b30d29922
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-04 16:08:35 +0300

    1 lesson - add Accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,19 @@ function Rating() {
     )
 }
 
+function Accordion() {
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+
+    )
+}
 
 /*
 // function expression
