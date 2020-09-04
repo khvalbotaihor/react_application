e101ca9f27ee7acd29e0d870eb15418240814054
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-04 23:25:51 +0300

    5 lesson - add OnOff component

---
+++ src/Components/OnOff/OnOff.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function OnOff() {
+    const onStyle = {
+
+    }
+    const offStyle = {
+
+    }
+    const cicleStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1 px solid black"
+    }
+
+
+
+return (
+    <div>
+        <div></div>
+        <div></div>
+        <div style={cicleStyle}></div>
+    </div>
+)
+}
+
+export default OnOff;
