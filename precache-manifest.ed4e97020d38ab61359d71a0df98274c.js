4c1b0d67412b71de195ffd4edd3e13db1f5dd1f4
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-09-04 10:24:40 +0300

    Updates

---
+++ precache-manifest.ed4e97020d38ab61359d71a0df98274c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459baefd9bab270957548b0258cc152c",
+    "url": "/react_application/index.html"
+  },
+  {
+    "revision": "44d0e29ca1b98359b0ae",
+    "url": "/react_application/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30e36a89ec21433b7a73",
+    "url": "/react_application/static/js/2.4bf17046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_application/static/js/2.4bf17046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d0e29ca1b98359b0ae",
+    "url": "/react_application/static/js/main.b5461f8d.chunk.js"
+  },
+  {
+    "revision": "384823682aa8766ac238",
+    "url": "/react_application/static/js/runtime-main.ce327cb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_application/static/media/logo.5d5d9eef.svg"
+  }
+]);
